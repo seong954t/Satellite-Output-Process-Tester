15fae5d3d5283eeb5d174fbcffb1f0eaaf34c7b5
Seoungtae Kim <seomtae@naver.com>
2018-12-06 15:11:02 +0900

    프로듀서 생성자 변경에 따른 실행 파일 변경

--- Mock_Satellite_Tester/main.py
+++ Mock_Satellite_Tester/main.py
@@ -4,7 +4,9 @@ import sys
 
 
 def main():
-    kafka_producer = SatelliteProducer(sys.argv[1], sys.argv[2], sys.argv[3])
+    # sys.argv[1] : server ip : port
+    # sys.argv[2] : topic
+    kafka_producer = SatelliteProducer(sys.argv[1], sys.argv[2])
     satellite_outer_producer = SatelliteOuterProducer(kafka_producer)
     while True:
         input_txt = input()
@@ -12,7 +14,7 @@ def main():
         mode = input_txt.split(' ')[1]
         if setting == 'stop':
             satellite_outer_producer.stop_file_scheduling(mode)
-        else:
+        elif setting == 'start':
             satellite_outer_producer.file_scheduling(mode)
 
 

ddc914a700fd16d81464f998116dd69c06b50214
Seoungtae Kim <seomtae@naver.com>
2018-12-06 13:34:59 +0900

    위성 산출기 기능 구현 완료

--- Mock_Satellite_Tester/satellite_outer.py
+++ Mock_Satellite_Tester/satellite_outer.py
@@ -1,25 +1,53 @@
 import datetime
+import os
 from threading import Timer
 
 
 class SatelliteOuter(object):
 
     def __init__(self):
-        self.FD = 10
-        self.LA = 3
-        self.file_scheduling(self.LA)
-        self.file_scheduling(self.FD)
+        self.mode_dic = {
+            'FD': {
+                'interval': 10,
+                'thread': Timer
+            },
+            'ELA': {
+                'interval': 5,
+                'thread': Timer
+            },
+            'LA': {
+                'interval': 3,
+                'thread': Timer
+            }
+        }
 
-    def file_scheduling(self, mode):
-        thread = Timer(mode-0.001, self.file_scheduling, [mode])
+    def file_scheduling(self, str_mode):
+        print(self.mode_dic[str_mode])
+        thread = Timer(self.mode_dic[str_mode]['interval']-0.001, self.file_scheduling, [str_mode])
         thread.start()
-        self.create_file(mode)
+        self.mode_dic[str_mode]['thread'] = thread
+        self.create_file(str_mode)
 
-    def create_file(self, mode):
-        if mode == self.LA :
-            print("LA : ", datetime.datetime.now())
-        else:
-            print("FD : ", datetime.datetime.now())
+    def stop_file_scheduling(self, str_mode):
+        self.mode_dic[str_mode]['thread'].cancel()
+        print(str_mode, ' is cancel')
 
+    def create_file(self, str_mode):
+        file_name = self.create_file_name(str_mode)
+        self.check_dir(file_name)
+        f = open(file_name, "w+")
+        f.write(str(datetime.datetime.now()))
+        f.close()
+        print(str_mode, ' : ', file_name)
 
-s = SatelliteOuter()
+    def create_file_name(self, str_mode):
+        file_name = "C:/Users/gsd/Desktop/COMS/{:s}/GOCI/%Y/%m/%d/COMS_GOCI_{:s}_%Y%m%d%H%M%S.txt"
+        file_name = datetime.datetime.strftime(datetime.datetime.now(), file_name)
+        file_name = file_name.format(str_mode, str_mode)
+        return file_name
+
+    @staticmethod
+    def check_dir(path):
+        path = '/'.join(path.split('/')[:-1])
+        if not os.path.exists(path):
+            os.makedirs(path)

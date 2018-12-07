df6bbead60dc3394bb674fd3e65179a128fc7762
Seoungtae Kim <seomtae@naver.com>
2018-12-07 17:09:15 +0900

    위성 산출 시간 설정 실행 추가

--- Mock_Satellite_Tester/main.py
+++ Mock_Satellite_Tester/main.py
@@ -12,9 +12,11 @@ def main():
         input_txt = input()
         setting = input_txt.split(' ')[0]
         mode = input_txt.split(' ')[1]
+        interval = input_txt.split(' ')[2]
         if setting == 'stop':
             satellite_outer_producer.stop_file_scheduling(mode)
         elif setting == 'start':
+            satellite_outer_producer.set_mode_interval(mode, interval)
             satellite_outer_producer.file_scheduling(mode)
 
 

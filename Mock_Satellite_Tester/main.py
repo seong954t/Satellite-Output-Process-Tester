f3f6e70e045479f670b3115bb5447f4d89ffe18a
Seoungtae Kim <seomtae@naver.com>
2018-12-10 13:05:58 +0900

    main flask 실행으로 변경

--- Mock_Satellite_Tester/main.py
+++ Mock_Satellite_Tester/main.py
@@ -1,24 +1,53 @@
-from satellite_producer import SatelliteProducer
-from satellite_outer_producer import SatelliteOuterProducer
+from Mock_Satellite_Tester.satellite_producer import SatelliteProducer
+from Mock_Satellite_Tester.satellite_outer_producer import SatelliteOuterProducer
 import sys
+from flask import Flask
+from flask_restful import Resource, Api
 
 
-def main():
-    # sys.argv[1] : server ip : port
-    # sys.argv[2] : topic
-    kafka_producer = SatelliteProducer(sys.argv[1], sys.argv[2])
-    satellite_outer_producer = SatelliteOuterProducer(kafka_producer)
-    while True:
-        input_txt = input()
-        setting = input_txt.split(' ')[0]
-        mode = input_txt.split(' ')[1]
-        interval = input_txt.split(' ')[2]
-        if setting == 'stop':
-            satellite_outer_producer.stop_file_scheduling(mode)
-        elif setting == 'start':
-            satellite_outer_producer.set_mode_interval(mode, interval)
-            satellite_outer_producer.file_scheduling(mode)
+app = Flask(__name__)
+api = Api(app)
 
+kafka_producer = SatelliteProducer(sys.argv[1], sys.argv[2])
+satellite_outer_producer = SatelliteOuterProducer(kafka_producer)
+
+
+class RunMode(Resource):
+
+    def get(self, mode, interval):
+        satellite_outer_producer.set_mode_interval(mode, interval)
+        satellite_outer_producer.file_scheduling(mode)
+        return 'OK'
+
+
+class StopMode(Resource):
+
+    def get(self, mode):
+        satellite_outer_producer.stop_file_scheduling(mode)
+        return 'OK'
+
+
+api.add_resource(RunMode, '/start/<mode>/<interval>')
+api.add_resource(StopMode, '/stop/<mode>/')
 
 if __name__ == '__main__':
-    main()
+     app.run(port='5002')
+# def main():
+#     # sys.argv[1] : server ip : port
+#     # sys.argv[2] : topic
+#     kafka_producer = SatelliteProducer(sys.argv[1], sys.argv[2])
+#     satellite_outer_producer = SatelliteOuterProducer(kafka_producer)
+#     while True:
+#         input_txt = input()
+#         setting = input_txt.split(' ')[0]
+#         mode = input_txt.split(' ')[1]
+#         interval = input_txt.split(' ')[2]
+#         if setting == 'stop':
+#             satellite_outer_producer.stop_file_scheduling(mode)
+#         elif setting == 'start':
+#             satellite_outer_producer.set_mode_interval(mode, interval)
+#             satellite_outer_producer.file_scheduling(mode)
+#
+#
+# if __name__ == '__main__':
+#     main()

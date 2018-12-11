d12d7aa4e6787a0b16d918fe4e1da99e60301d2a
Seoungtae Kim <seomtae@naver.com>
2018-12-11 11:32:07 +0900

    remove unused source in Mock_Satellite_Tester.main.py

--- Mock_Satellite_Tester/main.py
+++ Mock_Satellite_Tester/main.py
@@ -32,23 +32,3 @@ api.add_resource(StopMode, '/stop/<mode>/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5002')
-
-# def main():
-#     # sys.argv[1] : server ip : port
-#     # sys.argv[2] : topic
-#     kafka_producer = SatelliteProducer(sys.argv[1], sys.argv[2])
-#     satellite_outer_producer = SatelliteOuterProducer(kafka_producer)
-#     while True:
-#         input_txt = input()
-#         setting = input_txt.split(' ')[0]
-#         mode = input_txt.split(' ')[1]
-#         interval = input_txt.split(' ')[2]
-#         if setting == 'stop':
-#             satellite_outer_producer.stop_file_scheduling(mode)
-#         elif setting == 'start':
-#             satellite_outer_producer.set_mode_interval(mode, interval)
-#             satellite_outer_producer.file_scheduling(mode)
-#
-#
-# if __name__ == '__main__':
-#     main()

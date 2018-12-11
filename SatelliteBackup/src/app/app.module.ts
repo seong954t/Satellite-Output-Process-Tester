2e585ce2c74570f60f740cd3ea9b7a2ebf348157
kde0820 <kde0820@naver.com>
2018-12-11 17:03:48 +0900

    Firebase 연동

--- SatelliteBackup/src/app/app.module.ts
+++ SatelliteBackup/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { SatelliteBackupModule } from './satellite-backup/satellite-backup.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { SatelliteBackupComponent } from './satellite-backup/satellite-backup.component';
-import { DemoComponent } from './demo/demo.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
 import { FileViewerComponent } from './file-viewer/file-viewer.component';
+
 
 
 @NgModule({
@@ -17,6 +22,8 @@ import { FileViewerComponent } from './file-viewer/file-viewer.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
     SatelliteBackupModule,
     HttpClientModule
   ],

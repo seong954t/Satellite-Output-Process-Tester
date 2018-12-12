fcf3b86a68d9beab5dafc4129820372e6a4bef6f
kde0820 <kde0820@naver.com>
2018-12-12 16:04:55 +0900

    UI 변경 사항 적용

--- SatelliteBackup/src/app/satellite-backup/satellite-backup.component.ts
+++ SatelliteBackup/src/app/satellite-backup/satellite-backup.component.ts
@@ -9,45 +9,8 @@ export class SatelliteBackupComponent implements OnInit {
   @Input() recInfo;
   @Input() clusterInfo;
 
-  objectKeys = Object.keys;
-
   constructor() { }
 
   ngOnInit() {
-    this.initFirebase();
   }
-
-  clickRecMode(mode) {
-    if (this.recInfo[mode].running === true) {
-      this.modeTurnOff(mode);
-    } else {
-      this.modeTurnOn(mode);
-    }
-  }
-
-  modeTurnOn(mode) {
-    this.dataService.getStart(mode, this.recInfo[mode].interval).subscribe(x => {
-
-    });
-  }
-
-  modeTurnOff(mode) {
-    this.dataService.getStop(mode).subscribe(x => {
-
-    });
-  }
-
-  initFirebase() {
-    this.db.list('status').query.once('value').then(action => {
-      for (const key of Object.keys(action.val())) {
-        this.modeList[key] = false;
-      }
-      for (const [key, value] of Object.entries(this.modeList)) {
-        this.db.object(`status/${key}/satellite_file`).valueChanges().subscribe(val => {
-          console.log(val);
-        });
-      }
-    });
-  }
-
 }

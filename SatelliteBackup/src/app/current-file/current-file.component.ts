ea395468d6d09818219001c6e72e959f2f168282
kde0820 <kde0820@naver.com>
2018-12-12 13:59:24 +0900

    class 바인딩 추가

--- SatelliteBackup/src/app/current-file/current-file.component.ts
+++ SatelliteBackup/src/app/current-file/current-file.component.ts
@@ -14,44 +14,44 @@ export class CurrentFileComponent implements OnInit {
   SATELLITE_FILE = 0;
   SAVED_FILE = 1;
 
- constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase) { }
 
- ngOnInit() {
-   this.initFirebase();
- }
+  ngOnInit() {
+    this.initFirebase();
+  }
 
- initFirebase() {
-   this.db.list('status').query.once('value').then(action => {
-     for (const key of Object.keys(action.val())) {
-       this.modeList[key] = {
-         satellite_file: false,
-         saved_file: false
-       };
-     }
-     for (const [key, value] of Object.entries(this.modeList)) {
-       this.db.object(`status/${key}/satellite_file`).valueChanges().subscribe(val => {
-         console.log(key, value['satellite_file']);
-         this.modeList['satellite_file'] = true;
-         this.alertEndLatestFile(key, this.SATELLITE_FILE);
-       });
-       this.db.object(`status/${key}/saved_file`).valueChanges().subscribe(val => {
-         console.log(key, value['saved_file']);
-         this.modeList['saved_file'] = true;
-         this.alertEndLatestFile(key, this.SAVED_FILE);
-       });
-     }
-   });
- }
+  initFirebase() {
+    this.db.list('status').query.once('value').then(action => {
+      for (const key of Object.keys(action.val())) {
+        this.modeList[key] = {
+          satellite_file: false,
+          saved_file: false
+        };
+      }
+      for (const [key, value] of Object.entries(this.modeList)) {
+        this.db.object(`status/${key}/satellite_file`).valueChanges().subscribe(val => {
+          console.log(key, value['satellite_file']);
+          this.modeList[key]['satellite_file'] = true;
+          this.alertEndLatestFile(key, this.SATELLITE_FILE);
+        });
+        this.db.object(`status/${key}/saved_file`).valueChanges().subscribe(val => {
+          console.log(key, value['saved_file']);
+          this.modeList[key]['saved_file'] = true;
+          this.alertEndLatestFile(key, this.SAVED_FILE);
+        });
+      }
+    });
+  }
 
- alertEndLatestFile(mode: string, kind_of_file: number){
-   setTimeout(() => {
-     if(kind_of_file == this.SATELLITE_FILE){
-       this.modeList[mode]['satellite_file'] = false;
-     }else{
-       this.modeList[mode]['saved_file'] = false;
-     }
-   }, 1000);
+  alertEndLatestFile(mode: string, kind_of_file: number) {
+    setTimeout(() => {
+      if (kind_of_file === this.SATELLITE_FILE) {
+        this.modeList[mode]['satellite_file'] = false;
+      } else {
+        this.modeList[mode]['saved_file'] = false;
+      }
+    }, 1000);
 
- }
+  }
 
 }

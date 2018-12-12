b5db576ecd851c4e99635a9fe16380134b09ff42
kde0820 <kde0820@naver.com>
2018-12-12 14:07:32 +0900

    TreeView 수정

--- SatelliteBackup/src/app/file-viewer/file-viewer.component.ts
+++ SatelliteBackup/src/app/file-viewer/file-viewer.component.ts
@@ -8,109 +8,15 @@ import { of } from 'rxjs/observable/of';
   styleUrls: ['./file-viewer.component.css']
 })
 export class FileViewerComponent implements OnInit {
-  satellitePath = '/';
-  storePath = '/';
-  satelliteFileList: Array<String>;
-  storeFileList: Array<String>;
   satelliteDir: any;
   storeDir: any;
   satelliteRef: any;
   storeRef: AngularFireObject<{}>;
-  satelliteFirst = true;
-  storeFirst = true;
   satellitedata: any;
+  storedata: any;
 
   hasChildren = (item: any) => item.items && item.items.length > 0;
   children = (item: any) => of(item.items);
-
-  backDirectory(target: string) {
-    let currentPath = this.satellitePath;
-    let currentDir = this.satelliteDir;
-    if (target === 'store') {
-      currentPath = this.storePath;
-      currentDir = this.storeDir;
-    }
-    if (currentPath === '/') {
-      return;
-    }
-    let currentDirList = currentPath.slice(1, -1).split('/');
-    currentDirList = currentDirList.slice(0, -1);
-
-    if (currentDirList.length === 0) {
-      if (target === 'store') {
-        this.storePath = '/';
-        this.storeFileList = Object.keys(this.storeDir);
-      } else {
-        this.satellitePath = '/';
-        this.satelliteFileList = Object.keys(this.satelliteDir);
-      }
-      return;
-    }
-
-    for (const dir of currentDirList) {
-      if (dir !== '') {
-        if (target === 'store') {
-          this.storeFileList = this.setPostfix(currentDir[dir]);
-        } else {
-          this.satelliteFileList = this.setPostfix(currentDir[dir]);
-        }
-        currentDir = currentDir[dir];
-      }
-    }
-
-    if (target === 'store') {
-      this.storePath = '/' + currentDirList.join('/') + '/';
-    } else {
-      this.satellitePath = '/' + currentDirList.join('/') + '/';
-    }
-  }
-
-  changeDirectory(target: string, file: string) {
-    let currentPath = this.satellitePath;
-    let currentDir = this.satelliteDir;
-    if (target === 'store') {
-      currentPath = this.storePath;
-      currentDir = this.storeDir;
-    }
-    const currentDirList = currentPath.trim().split('/');
-    currentDirList.push(file);
-
-    for (const dir of currentDirList) {
-      if (dir !== '') {
-        if (currentDir[dir] === undefined) {
-          return;
-        }
-        if (target === 'store') {
-          this.storeFileList = this.setPostfix(currentDir[dir]);
-        } else {
-          this.satelliteFileList = this.setPostfix(currentDir[dir]);
-        }
-        currentDir = currentDir[dir];
-      }
-    }
-
-    if (file !== '') {
-      if (target === 'store') {
-        this.storePath = currentPath + file + '/';
-      } else {
-        this.satellitePath = currentPath + file + '/';
-      }
-    }
-
-  }
-
-  setPostfix(currentDir): Array<String> {
-    const currentList = Object.keys(currentDir);
-    for (let i = 0; i < currentList.length; i++) {
-      const key = currentList[i];
-      if (typeof (currentDir[key]) === 'string') {
-        currentList[i] = key + '.' + currentDir[key];
-      } else {
-        break;
-      }
-    }
-    return currentList;
-  }
 
   constructor(private db: AngularFireDatabase) {
     // satellite
@@ -118,24 +24,13 @@ export class FileViewerComponent implements OnInit {
     this.satelliteRef.snapshotChanges().subscribe(action => {
       this.satelliteDir = action.payload.val();
       this.satellitedata = this.getTreeViewDataStructure(this.satelliteDir);
-      if (this.satelliteFirst) {
-        this.satelliteFileList = Object.keys(this.satelliteDir);
-        this.satelliteFirst = false;
-      } else {
-        this.changeDirectory('satellite', '');
-      }
     });
 
     // store
     this.storeRef = this.db.object('saved_dir');
     this.storeRef.snapshotChanges().subscribe(action => {
       this.storeDir = action.payload.val();
-      if (this.storeFirst) {
-        this.storeFileList = Object.keys(this.storeDir);
-        this.storeFirst = false;
-      } else {
-        this.changeDirectory('store', '');
-      }
+      this.storedata = this.getTreeViewDataStructure(this.storeDir);
     });
   }
 

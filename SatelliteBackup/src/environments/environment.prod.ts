e4f5798f445006721befd3aabca14b63d44c9641
kde0820 <kde0820@naver.com>
2018-12-13 16:44:12 +0900

    firebase config 수정

--- SatelliteBackup/src/environments/environment.prod.ts
+++ SatelliteBackup/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCHMfRCIynLfz12UUHAu2Wb_F9t1l3Rve0",
-    authDomain: "satellite-d94ef.firebaseapp.com",
-    databaseURL: "https://satellite-d94ef.firebaseio.com",
-    projectId: "satellite-d94ef",
-    storageBucket: "satellite-d94ef.appspot.com",
-    messagingSenderId: "886639049701"
+    apiKey: {my-apiKey},
+    authDomain: {my-authDomain},
+    databaseURL: {my-databaseURL},
+    projectId: {my-projectId},
+    storageBucket: {my-storageBucket},
+    messagingSenderId: {my-messagingSenderId}
   }
 };

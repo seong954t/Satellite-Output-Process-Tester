eb38e7ce40baebae68bf57bf388712574f6cbb1e
Seoungtae Kim <seomtae@naver.com>
2018-12-14 10:21:19 +0900

    Test Producer 삭제

--- Kafka_Consumer_Saver/Producer1.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaProducer
-
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-producer.send('RsyncTopic', value=b'Execute Rsync').get(timeout=30)
-print("send message")
-
